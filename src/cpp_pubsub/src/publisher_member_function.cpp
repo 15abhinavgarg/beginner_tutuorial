7821ceff02d7362a5d4953cfa6f4c5842b9bd1f1
fierra <15abhinavgarg@gmail.com>
2022-11-17 17:21:15 -0500

    String changed

--- src/cpp_pubsub/src/publisher_member_function.cpp
+++ src/cpp_pubsub/src/publisher_member_function.cpp
@@ -40,7 +40,7 @@ private:
   void timer_callback()
   {
     auto message = std_msgs::msg::String();
-    message.data = "Abhinav first ros project" + std::to_string(count_++);
+    message.data = "This is the change in string" + std::to_string(count_++);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
   }
